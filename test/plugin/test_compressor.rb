cdfe1e4c09732f050763d94f5b0818775665c6e6
okkez <okkez000@gmail.com>
2016-05-27 16:46:39 +0900

    Use `generate_chunk` like method instead of chunk related class

--- test/plugin/test_compressor.rb
+++ test/plugin/test_compressor.rb
@@ -4,8 +4,19 @@ require "snappy"
 
 class CompressorTest < Test::Unit::TestCase
   class Snappy < self
+
+    CONFIG = %[
+      host namenode.local
+      path /hdfs/path/file.%Y%m%d.log
+    ]
+
     def setup
+      Fluent::Test.setup
       @compressor = Fluent::WebHDFSOutput::SnappyCompressor.new
+    end
+
+    def create_driver(conf=CONFIG,tag='test')
+      Fluent::Test::OutputTestDriver.new(Fluent::WebHDFSOutput, tag).configure(conf)
     end
 
     def test_ext
@@ -13,8 +24,21 @@ class CompressorTest < Test::Unit::TestCase
     end
 
     def test_compress
-      chunk = Fluent::MemoryBufferChunk.new("test")
-      chunk << "hello snappy\n" * 32 * 1024
+      d = create_driver
+      if d.instance.respond_to?(:buffer)
+        buffer = d.instance.buffer
+      else
+        buffer = d.instance.instance_variable_get(:@buffer)
+      end
+
+      if buffer.respond_to?(:generate_chunk)
+        chunk = buffer.generate_chunk("test")
+        chunk.concat("hello snappy\n" * 32 * 1024, 1)
+      else
+        chunk = buffer.new_chunk("test")
+        chunk << "hello snappy\n" * 32 * 1024
+      end
+
       io = Tempfile.new("snappy-")
       @compressor.compress(chunk, io)
       io.open
